e1a9a743d54521a352600a8c2d86f59c5fc297ff
Squaretrix <patrick.stadler01@gmail.com>
2018-10-01 19:53:22 +0200

    V1.01 Added: Coments

--- src/MainClass.java
+++ src/MainClass.java
@@ -4,9 +4,10 @@ public class MainClass {
 	public static void main(String[] args) {
 		WorkshopMethods master = new WorkshopMethods();
 
-		char[][] player = master.activate(5, 5);
+		char[][] player = master.activate(38, 7);		//(length, width)
 
 		master.print(player);
+		
 	}
 
 }
