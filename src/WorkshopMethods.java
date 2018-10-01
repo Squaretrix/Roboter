62b983053e070e6093f13ece1eb77e0722f583f1
Squaretrix <patrick.stadler01@gmail.com>
2018-10-01 19:29:04 +0200

    V1.01 Added: OBSTACLES spawn around the field

--- src/WorkshopMethods.java
+++ src/WorkshopMethods.java
@@ -5,10 +5,11 @@ public class WorkshopMethods {
 	final char WAY = 'W';
 
 	public char[][] activate(int width, int length) {
-		char[][] playfield = new char[width][length];
+		char[][] playfield = new char[length][width];
 		for (int i = 0; i < playfield[0].length; i++) {
 			for (int j = 0; j < playfield.length; j++) {
 				playfield[j][i] = WAY;
+				setObstacle(playfield);
 			}
 		}
 		return playfield;
@@ -17,11 +18,29 @@ public class WorkshopMethods {
 	public char[][] print(char[][] playfield) {
 		for (int i = 0; i < playfield[0].length; i++) {
 			for (int j = 0; j < playfield.length; j++) {
-				System.out.print(playfield[j][i]+" ");
+				System.out.print(playfield[j][i] + " ");
 			}
 			System.out.println(" ");
 		}
 		return playfield;
 	}
-	public void 
+
+	public void setObstacle(char[][] playfield) {
+		// left -> right top
+		for (int i = 0; i < playfield.length; i++) {
+			playfield[i][0] = OBSTACLE;
+		}
+		// left -> left down
+		for (int i = 0; i < playfield[0].length; i++) {
+			playfield[0][i] = OBSTACLE;
+		}
+		// right top -> right down
+		for (int i = 0; i < playfield.length; i++) {
+			playfield[playfield[0].length - 1][i] = OBSTACLE;
+		}
+		// left -> right bottom
+		for (int i = 0; i < playfield.length; i++) {
+			playfield[i][playfield.length-1] = OBSTACLE;
+		}
+	}
 }
